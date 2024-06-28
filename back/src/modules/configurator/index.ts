e644a959d012c94860095cd34cfc674429a04f5e
kbrdn1 <kylianb1@icloud.com>
2024-06-28 16:56:37 +0200

    🔥 chore(back)!: rm dead code

--- back/src/modules/configurator/index.ts
+++
@@ -1,3 +0,0 @@
-// Configurator module exports - index.ts
-export { default as ConfiguratorService } from "./configurator.service";
-export { default as ConfiguratorController } from "./configurator.controller";
